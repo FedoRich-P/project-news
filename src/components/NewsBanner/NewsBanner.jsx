e10bed9d5203c70cf8823baac8bfd9c2f0111ee2
FedoRich-P <panchev_fedor@mail.ru>
2025-01-26 16:04:39 +0300

    refactoring

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,14 +1,16 @@
 import s from './NewsBanner.module.scss';
 import { formatTimeAgo } from '../../helpers/formatTimeAgo.js';
 import { Image } from '../Image/Image.jsx';
+import { withSkeleton } from '../../helpers/hocs/withSkeleton.jsx';
 
-export const NewsBanner = (item) => {
-return (
-  <article className={s.baner}>
-    <Image image={item?.image} />
-    <h3>{item?.title}</h3>
-    <p>{formatTimeAgo(item?.published)} by {item?.author}</p>
-  </article>
-)
-
+const NewsBanner = ({ item }) => {
+  return (
+    <article className={s.banner}>
+      <Image image={item?.image} />
+      <h3>{item?.title}</h3>
+      <p>{formatTimeAgo(item?.published)} by {item?.author}</p>
+    </article>
+  );
 };
+
+export const NewsBannerWithSkeleton = withSkeleton(NewsBanner, 'banner', 1)
--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,14 +1,15 @@
 import s from './NewsList.module.scss';
-import { formatTimeAgo } from '../../helpers/formatTimeAgo.js';
 import { NewsItem } from '../NewsItem/NewsItem.jsx';
+import { withSkeleton } from '../../helpers/hocs/withSkeleton.jsx';
 
-export const NewsList = ({ news }) => {
-return (
-  <ul className={s.list}>
-    {news && news.map((item) => (
-      <NewsItem key={item.id} item={item} />
-    ))}
-  </ul>
-)
-
+const NewsList = ({ news }) => {
+  return (
+    <ul className={s.list}>
+      {news && news.map((item) => (
+        <NewsItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
 };
+
+export const NewsListWithSkeleton = withSkeleton(NewsList, 'item', 10);
